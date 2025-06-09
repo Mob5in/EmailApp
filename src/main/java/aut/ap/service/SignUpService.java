e1b5f59bb64af7ef09858dd764ff98c887b4bc09
Mobin <Mobinmojiri1234@gmail.com>
2025-06-10 02:47:04 +0330

    compliting signupservice

---
+++ src/main/java/aut/ap/service/SignUpService.java
@@ -0,0 +1,43 @@
+package aut.ap.service;
+import aut.ap.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import static aut.ap.graphic.PopUp.pupUp;
+
+
+
+
+public class SignUpService {
+    private static org.hibernate.SessionFactory sessionFactory;
+    private static void setUpSessionFactory() {
+        sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    private static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+
+    public static void register(String name, String email, String password){
+        try{
+            System.out.println("hello");
+            User user = new User(name, email, password);
+            setUpSessionFactory();
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.persist(user);
+            tx.commit();
+        }catch (Exception e){
+                String errorMassage = e.getMessage();
+                pupUp(errorMassage);
+        }finally {
+            closeSessionFactory();
+        }
+
+    }
+
+
+}
