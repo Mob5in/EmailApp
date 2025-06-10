f23ce8038081b9e5bf57b2201c17814ec922de02
Mobin <Mobinmojiri1234@gmail.com>
2025-06-10 03:40:13 +0330

    coding login similar to signUp

---
+++ src/main/java/aut/ap/graphic/Login.java
@@ -0,0 +1,63 @@
+package aut.ap.graphic;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static aut.ap.service.SignUpService.register;
+
+public class Login {
+    public static ActionListener login() {
+        JFrame frame = new JFrame("Login - Milou");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.setBackground(Color.WHITE);
+
+        JLabel emailLabel = new JLabel("Email:");
+        JTextField emailField = new JTextField();
+        emailField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        submitButton.setMaximumSize(new Dimension(150, 40));
+
+
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+        panel.add(emailLabel);
+        panel.add(emailField);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(Box.createRigidArea(new Dimension(0, 20)));
+        panel.add(submitButton);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = emailField.getText();
+                String password = new String(passwordField.getPassword());
+
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
+
+            }
+        });
+
+        frame.add(panel);
+        frame.setVisible(true);
+        return null;
+    }
+
+
+}
