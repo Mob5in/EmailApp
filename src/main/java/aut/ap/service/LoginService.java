346a005baca4fc99c4e938884f5b3009f8b94ba3
Mobin <Mobinmojiri1234@gmail.com>
2025-06-10 03:44:50 +0330

    coding login, importing code from last project

---
+++ src/main/java/aut/ap/service/LoginService.java
@@ -0,0 +1,45 @@
+package aut.ap.service;
+
+import aut.ap.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import static aut.ap.graphic.PopUp.pupUp;
+
+public class LoginService {
+    private static SessionFactory sessionFactory;
+
+
+    private static void setUpSessionFactory() {
+        sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    private static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+
+    public static void login(String email, String password) {
+        setUpSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.createQuery("FROM User WHERE email = :email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResult();
+
+            if (user == null) {
+                pupUp("Doesn't exist!");
+            } else if (user.getPassword().equals(password)) {
+                pupUp("Welcome, " + user.getName());
+            } else {
+                pupUp("Password is incorrect!");
+            }
+        } catch (Exception e) {
+            pupUp("Error during login: " + e.getMessage());
+        } finally {
+            closeSessionFactory();
+        }
+    }
+}
