8112a2c59fccb39af6713478826d1d7a8dae1bfe
Mobin <Mobinmojiri1234@gmail.com>
2025-06-11 02:45:58 +0330

    coding read UnreadEmails just like ReadAllEmails

---
+++ src/main/java/aut/ap/graphic/userEnvironment/Views/UnreadEmails.java
@@ -0,0 +1,67 @@
+package aut.ap.graphic.userEnvironment.Views;
+
+import aut.ap.model.Email;
+import aut.ap.service.GetEmailsService;
+import aut.ap.service.GetQueryService;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class UnreadEmails {
+
+    public static void ShowUnreadEmails() {
+        JFrame frame = new JFrame("All Emails - Milou");
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(Color.PINK);
+        String query = "SELECT er.email FROM EmailRecipient er WHERE er.recipient.id = :userId AND er.isRead = false";
+        List<Email> unreadEmail = GetQueryService.getQuery(query);
+
+
+
+        JPanel emailListPanel = new JPanel();
+        emailListPanel.setLayout(new BoxLayout(emailListPanel, BoxLayout.Y_AXIS));
+        emailListPanel.setBackground(Color.WHITE);
+        emailListPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        if (unreadEmail.isEmpty()) {
+            JLabel emptyLabel = new JLabel("There is no email");
+            emptyLabel.setFont(new Font("Arial", Font.ITALIC, 14));
+            emptyLabel.setForeground(Color.GRAY);
+            emptyLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            emailListPanel.add(emptyLabel);
+        } else {
+            for (Email email : unreadEmail) {
+                JPanel emailPanel = new JPanel();
+                emailPanel.setLayout(new GridLayout(3, 1));
+                emailPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+                emailPanel.setBackground(Color.WHITE);
+
+                JLabel senderLabel = new JLabel("Sender: " + email.getSender().getName());
+                JLabel subjectLabel = new JLabel("Subject: " + email.getSubject());
+                JLabel dateLabel = new JLabel("Date: " + email.getSentDate());
+                JLabel bodyLabel = new JLabel("Massage : " + email.getBody() );
+
+                emailPanel.add(senderLabel);
+                emailPanel.add(subjectLabel);
+                emailPanel.add(dateLabel);
+                emailPanel.add(bodyLabel);
+
+                emailListPanel.add(emailPanel);
+                emailListPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+
+        JScrollPane scrollPane = new JScrollPane(emailListPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+
+}
