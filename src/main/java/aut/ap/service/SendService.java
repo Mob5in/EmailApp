fe0713a796e595fad01130aa3c879f7450ed2e8a
Mobin <Mobinmojiri1234@gmail.com>
2025-06-10 20:33:11 +0330

    completing email send service

--- src/main/java/aut/ap/service/SendService.java
+++ src/main/java/aut/ap/service/SendService.java
@@ -1,22 +1,59 @@
 package aut.ap.service;
-
 import aut.ap.graphic.PopUpWindow;
 import aut.ap.graphic.userEnvironment.MenuWindow;
 import aut.ap.model.EmailRecipient;
 import aut.ap.model.Emails;
 import aut.ap.model.User;
-import org.hibernate.id.enhanced.PooledLoOptimizer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import static aut.ap.graphic.PopUpWindow.pupUp;
 
 public class SendService{
 
+    private static org.hibernate.SessionFactory sessionFactory;
+    private static void setUpSessionFactory() {
+        sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    private static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+
+
     public static void sendEmail(String subject, String recipient, String body){
-        if(recipient.equals("")){
+        if(recipient.isEmpty()){
             PopUpWindow.pupUp("recipient canot be empty");
+            return;
         }
         User user = MenuWindow.getCorrentUser();
-        User userRecipient = GetUserService.getUser(recipient);
+        User recipientUser = GetUserService.getUser(recipient);
         Emails email = new Emails(user, subject, body);
-        EmailRecipient emailRecipient = new EmailRecipient(email, userRecipient);
+        EmailRecipient emailRecipient = new EmailRecipient(email, recipientUser);
+
+        if (recipientUser == null) {
+            PopUpWindow.pupUp("Recipient not found");
+            return;
+        }
+
+
+        try{
+            setUpSessionFactory();
+            Session session = sessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.persist(email);
+            session.persist(emailRecipient);
+            tx.commit();
+            pupUp("Email send succesfully");
+        }catch (Exception e){
+            String errorMassage = e.getMessage();
+            pupUp(errorMassage);
+        }finally {
+            closeSessionFactory();
+        }
 
     }
 
