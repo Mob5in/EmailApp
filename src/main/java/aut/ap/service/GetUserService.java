891d8e7cb1d31b8c7d2d5de9bf0f364e6562f4a3
Mobin <Mobinmojiri1234@gmail.com>
2025-06-10 20:20:07 +0330

    coding get user for email works

---
+++ src/main/java/aut/ap/service/GetUserService.java
@@ -0,0 +1,47 @@
+package aut.ap.service;
+import aut.ap.model.Emails;
+import aut.ap.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import static aut.ap.graphic.PopUpWindow.pupUp;
+
+public class GetUserService {
+
+    private static SessionFactory sessionFactory;
+
+
+    private static void setUpSessionFactory() {
+        sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    private static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+
+    public static User getUser(String email) {
+
+        setUpSessionFactory();
+
+        try (
+                Session session = sessionFactory.openSession()) {
+            User user = session.createQuery("FROM User WHERE email = :email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResult();
+
+            if (user == null) {
+                pupUp("Doesn't exist such user\n" +
+                        "email canot sent, try again!");
+            } else {
+                return user;
+            }
+        } catch (Exception e) {
+            pupUp("Error during searching: " + e.getMessage());
+        } finally {
+            closeSessionFactory();
+        }
+        return null;
+    }
+}
